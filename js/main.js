035f67f43f983c3bd310234b86af2bdae8a2a108
André devkcud Albanese <patommmmm@proton.me>
2023-11-20 18:33:33 -0300

    add comments

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 import { addTodo } from "./todo.js";
 import { render } from "./render.js";
 
+// Seleciona o elemento form
 const form = document.querySelector("form");
 
+// Adiciona um listener para o evento submit (enviar dados)
 form.addEventListener("submit", (event) => {
+  // Previne o comportamento padrão
   event.preventDefault();
 
+  // Seleciona o elemento input (a descrição que o usuario digitou)
   const input = form.querySelector("input");
 
+  // Adiciona a tarefa
   addTodo(input.value);
 
+  // Limpa o input
   input.value = "";
 
+  // Atualiza a visualização
   render();
 });
 
--- js/render.js
+++ js/render.js
@@ -1,51 +1,81 @@
 import { __updateLocalStorage, getTodo, removeTodo } from "./todo.js";
 
+/** Cria uma LI com os dados de uma tarefa
+ *
+ * @param {string} uuid - O ID da tarefa.
+ * @param {object} todo - Os dados da tarefa
+ *
+ * @returns {HTMLLIElement} O elemento LI criado
+ */
 function createListItem(uuid, todo) {
+  // Cria o elemento LI
   const item = document.createElement("li");
 
+  // Cria os elementos input
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.checked = todo.done;
+  // Atualiza o estado da tarefa
   checkbox.addEventListener("change", () => {
     todo.done = checkbox.checked;
     __updateLocalStorage();
+    // Atualiza a visualização
     render();
   });
 
+  // Cria o elemento de descrição
+  // A descrição pode ser alterada
   const description = document.createElement("input");
   description.value = todo.description;
+  // Atualiza a descrição da tarefa
   description.addEventListener("change", () => {
     todo.description = description.value;
     __updateLocalStorage();
+    // Não há necessidade de atualizar a visualização
   });
 
+  // Cria o elemento de botão de remover
   const button = document.createElement("button");
   button.textContent = "X";
-
+  // Remove o item da lista
   button.addEventListener("click", () => {
     removeTodo(uuid);
     render();
   });
 
+  // Adiciona os elementos criados ao elemento LI
   item.append(checkbox, description, button);
 
+  // Retorna o elemento LI
   return item;
 }
 
+/** Renderiza a lista de tarefas
+ *
+ * @returns {void}
+ */
 function render() {
+  // Seleciona as listas
   const todoList = document.querySelector("ul#todo");
   const doneList = document.querySelector("ul#done");
 
+  // Limpa as listas
   todoList.innerHTML = "";
   doneList.innerHTML = "";
 
+  // Adiciona os elementos
+  // Object.keys(getTodo()) -> Retorna um array com os IDs
   for (const uuid of Object.keys(getTodo())) {
+    // Pega a tarefa pelo ID
     const todo = getTodo(uuid);
 
+    // Cria o elemento LI
     const item = createListItem(uuid, todo);
 
+    // Adiciona o elemento LI em uma das listas de acordo com o estado
     (todo.done ? doneList : todoList).append(item);
   }
 }
 
+// Exportação de funções
 export { render };
